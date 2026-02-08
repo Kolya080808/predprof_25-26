98e409904a8f0cf4c0015a2db66d0a7dfcd0c35e
Nikolay <81642661+Kolya080808@users.noreply.github.com>
2026-02-08 16:06:51 +0300

    Update transmitter code for button and telemetry
    Refactored button handling and telemetry structure.

--- Программный код/transmitter.ino
+++ Программный код/transmitter.ino
@@ -2,60 +2,58 @@
 #include <RF24.h>
 
 RF24 radio(9, 10);
-const uint8_t buttonPin = 2;
+const int buttonPin = 2;
 
 const byte addressTx[] = "CMD01";
 const byte addressRx[] = "DATA1";
 
-struct CommandPacket {
-  uint8_t command;
-};
+byte lastbuttonState = 0;
+byte nowbuttonstate = 0;
+int timetopress = 50;
+
+char messange[] = "start";
 
 struct TelemetryPacket {
-  uint8_t id;
-  int8_t pitch;
-  int8_t yaw;
-  uint8_t mode;
+  char id[4];
+  char mode[2];
+  char pitch[4];
+  char yaw[4];
 };
 
-CommandPacket cmd;
 TelemetryPacket telemetry;
 
 void setup() {
   Serial.begin(9600);
-  pinMode(buttonPin, INPUT_PULLUP);
-  
+  pinMode(buttonPin, INPUT);
   radio.begin();
   radio.setPALevel(RF24_PA_LOW);
   radio.setDataRate(RF24_1MBPS);
-  
   radio.openWritingPipe(addressTx);
   radio.openReadingPipe(1, addressRx);
-  
   radio.startListening();
 }
 
 void loop() {
-  if (digitalRead(buttonPin) == LOW) {
-    delay(50);
-    if (digitalRead(buttonPin) == LOW) {
+  nowbuttonstate = digitalRead(buttonPin);
+  if(nowbuttonstate && nowbuttonstate != lastbuttonState ){
+    if(millis() - timetopress > 50){
+      timetopress = millis();
       Serial.println("Button pressed. Sending START.");
-      
       radio.stopListening();
-      cmd.command = 1;
-      radio.write(&cmd, sizeof(cmd));
+      bool ok = radio.write(&messange, sizeof(messange));
+      if (!ok) Serial.println("Ошибка: команда не отправлена!");
       radio.startListening();
-      
-      delay(1000);
     }
   }
-  
+  lastbuttonState = nowbuttonstate;
+
   if (radio.available()) {
-    radio.read(&telemetry, sizeof(telemetry));
+    TelemetryPacket newTel;
+    radio.read(&newTel, sizeof(newTel));
+    Serial.print("RECV | ID: "); Serial.print(newTel.id);
+    Serial.print(" | Mode: "); Serial.print(newTel.mode);
+    Serial.print(" | Pitch: "); Serial.print(newTel.pitch);
+    Serial.print(" | Yaw: "); Serial.println(newTel.yaw);
     
-    Serial.print("RECV | ID: "); Serial.print(telemetry.id);
-    Serial.print(" | Mode: "); Serial.print(telemetry.mode);
-    Serial.print(" | Pitch: "); Serial.print(telemetry.pitch);
-    Serial.print(" | Yaw: "); Serial.println(telemetry.yaw);
   }
 }
